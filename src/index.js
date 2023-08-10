9fd4befd55bd4e11d53ed6fc016709a77b243d36
priyanshsingh <priyanshsingh1401@gmail.com>
2023-08-10 19:29:56 +0530

    changed code

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./context/AuthContext";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Route, BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/stripe-frontend">
       <AuthProvider>
         <App />
       </AuthProvider>
